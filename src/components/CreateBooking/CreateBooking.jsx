689b8500610d769d34cb1a28143a0e1a6d1b4d05
Martin <saadv89@gmail.com>
2026-01-20 09:48:31 +0100

    Created a create booking component

--- src/components/CreateBooking/CreateBooking.jsx
+++ src/components/CreateBooking/CreateBooking.jsx
@@ -1,5 +1,71 @@
+import { useEffect, useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import "./calendar.css";
 import "./CreateBooking.css";
+import TimeSlot from "../TimeSlot/TimeSlot";
 
 export default function CreateBooking() {
-  return <>Hej</>;
+  const [timeSlots, setTimeSlots] = useState([]);
+  const [value, setValue] = useState(new Date());
+  const [timeSlotId, setTimeSlotId] = useState();
+  const [scheduleId, setScheduleId] = useState();
+  const [comment, setComment] = useState("");
+  const dateStr = value.getFullYear() + "-" + String(value.getMonth() + 1).padStart(2, "0") + "-" + value.getDate();
+  const today = new Date();
+  today.setDate(today.getDate() - 1);
+
+  useEffect(() => {
+    async function fetchTimeSlots() {
+      const result = await fetch(`${import.meta.env.VITE_BASE_URL}Booking/available-timeslots?date=${dateStr}`); // flex date
+      const data = await result.json();
+      setTimeSlots(data);
+    }
+    fetchTimeSlots();
+  }, [value]);
+
+  const timeSlot = timeSlots.find((ts) => ts?.["id"] === timeSlotId);
+  const handleBooking = async () => {
+    // const dateStr = value.getFullYear() + "-" + String(value.getMonth() + 1).padStart(2, "0") + "-" + value.getDate();
+    const request = {
+      comment: comment,
+      start: timeSlot.start,
+      date: dateStr,
+      timeSlotId: timeSlotId,
+      scheduleId: scheduleId,
+    };
+
+    const result = await fetch(`${import.meta.env.VITE_BASE_URL}Booking`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(request),
+    });
+
+    const data = await result.json();
+    console.log(data);
+  };
+
+  return (
+    <div className="create-booking-container">
+      <h1 className="create-booking-container__title">BOKA TID</h1>
+      {/* <select name="" id=""></select> */}
+      <div className="create-booking-container__calender">
+        <div className="calendar-wrapper">
+          <Calendar onChange={setValue} value={value} locale="sv-SE" tileDisabled={({ date }) => date < today} />
+        </div>
+      </div>
+      <div className="create-booking-container__time-slots">
+        {timeSlots.map((ts) => (
+          <TimeSlot props={{ ts, value, setTimeSlotId, setScheduleId }} />
+        ))}
+      </div>
+      <textarea className="create-booking-container__text-area" name="" id=""></textarea>
+      <button className="create-booking-container__create-btn" onClick={handleBooking}>
+        Boka
+      </button>
+    </div>
+  );
 }
